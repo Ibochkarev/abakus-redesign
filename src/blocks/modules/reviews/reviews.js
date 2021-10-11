dcb69315fd168f29dfce44bbc43c52d366ac3ab7
Bochkarev Ivan <ivanx86@gmail.com>
2021-10-12 01:21:57 +0600

    Обновление отзывов

--- src/blocks/modules/reviews/reviews.js
+++ src/blocks/modules/reviews/reviews.js
@@ -1,4 +1,8 @@
-const sectionReviewsReview = document.querySelectorAll('.section-reviews__review');
+const sectionReviewsReview = document.querySelectorAll(".section-reviews__review");
+const sectionReviewsWrapper = document.querySelector(".section-reviews__wrapper");
+
+const sectionReviewsItemText = sectionReviewsWrapper.querySelectorAll(".section-reviews__item--text");
+const sectionReviewsWrapViseo = sectionReviewsWrapper.querySelectorAll(".section-reviews__item--wrap-video");
 
 if(sectionReviewsReview) {
     sectionReviewsReview.forEach(function(elem, i) {
@@ -7,5 +11,15 @@ if(sectionReviewsReview) {
         if( text.length <= limit) return text;
         text = text.slice(0, limit);
         elem.innerHTML = text.trim() + "...";
-    })
+    });
+}
+
+if(sectionReviewsWrapper) {
+    sectionReviewsItemText.forEach((elem) => {
+        if(!elem.innerHTML)elem.classList.add("hide-elem");
+    });
+
+    sectionReviewsWrapViseo.forEach((elem) => {
+        if(!elem.innerHTML)elem.classList.add("hide-elem");
+    });
 }
